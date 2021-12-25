f6b44b0b78a157266118e802a9158270a20b07c9
Vyacheslav <dalealrussia@gmail.com>
2021-12-26 02:38:43 +0300

    Working with Files

---
+++ Working-with-Files/main.py
@@ -0,0 +1,30 @@
+import os
+
+
+ans = input("Hello! How can I help you?\n"
+      "\n1. Create file"
+      "\n2. Delete file"
+      "\n3. Create folder"
+      "\n4. Delete folder\n\n")
+
+
+if ans == "1":
+    name = input("Write name of file: ")
+
+    file = open(name, "w+")
+    file.close()
+
+elif ans == "2":
+    name = input("Write name of file: ")
+
+    os.remove(name)
+
+elif ans == "3":
+    name = input("Write name of folder: ")
+
+    os.makedirs(name, exist_ok=True)
+
+elif ans == "4":
+    name = input("Write name of folder: ")
+
+    os.rmdir(name)
