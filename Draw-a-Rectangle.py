7aa797cbf3ee06ec4e8810c6ee6e454ec1a8903c
Vyacheslav <dalealrussia@gmail.com>
2022-01-27 00:22:49 +0300

    Create Draw-a-Rectangle.py

---
+++ Draw-a-Rectangle.py
@@ -0,0 +1,15 @@
+# https://github.com/justsyntex
+
+
+while True:
+    width = int(input("Enter the width: "))
+    height = int(input("Enter the height: "))
+
+    print("")
+
+    i = 0
+    j = 0
+
+    while i < height:
+        print("*" * width)
+        i += 1
